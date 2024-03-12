99d4ceefd7178863d4b643508de562765619b0f0
Kaushley Mehra <kaushley14@gmail.com>
2024-03-12 18:16:02 +0530

    FIrst committ

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-gray-200 p-4">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
